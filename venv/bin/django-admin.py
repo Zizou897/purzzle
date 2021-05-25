fd8a8757f01734c84cf01668b478358b31c054f7
Zizou897 <azeridwan10@gmail.com>
2021-05-25 16:47:56 +0000

    traite

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/zizou/Documents/GitHub/purzzle/venv/bin/python3
+#!/Users/ecolenan/Desktop/GitHub/purzzle/venv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
